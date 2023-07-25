d3a695ce788fdce34d301b24d8470371a28ac211
Afonso-Carvalho <afonsox57@gmail.com>
2023-07-24 21:18:52 -0300

    Adicionando CORS

--- setup/settings.py
+++ setup/settings.py
@@ -42,12 +42,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    "corsheaders",
     'jornada_milhas',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -138,3 +140,7 @@ REST_FRAMEWORK = {
     #    ],
     #'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.BasicAuthentication',]
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
